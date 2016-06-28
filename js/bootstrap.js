ec48ce8bbd6bdfa664e51c21e990b5d0cd35ad76
TheHermitKhan <leftpheildradio@gmail.com>
2016-06-28 14:57:26 -0400

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2411,10 +2411,11 @@ $('.tab a').on('click', function (e) {
 
 // Added Analytics tracking
 //=========================
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  $(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-60695320-1', 'auto');
   ga('send', 'pageview');
+  });
