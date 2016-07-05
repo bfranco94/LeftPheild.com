d2854b3c2486b2441fa9b2421414aff8a3858187
TheHermitKhan <leftpheildradio@gmail.com>
2016-07-04 22:36:29 -0400

    update

--- js/dropdown.js
+++ js/dropdown.js
@@ -6,7 +6,7 @@
   $btn.on('click', function() {
     var isOpen = $ul.is(':visible'),
         slideDir = isOpen ? 'slideUp' : 'slideDown',
-        btnText = isOpen ? ogText : 'Bring It Back Up',
+        btnText = isOpen ? ogText : 'Close',
         dur = isOpen ? 200 : 400;
     $ul.velocity(slideDir, {
       easing: 'easeOutQuart',
