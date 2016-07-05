5bf91db4604125a438075b60fc8b2e0f647c43b9
TheHermitKhan <leftpheildradio@gmail.com>
2016-07-05 16:20:35 -0400

    Update lights.js

--- js/lights.js
+++ js/lights.js
@@ -1,4 +1,23 @@
-
+(function() {
+  var $btn = $('#dd-btn'),
+      $ul = $btn.next('ul'),
+      ogText = $btn.text();
+  
+  $btn.on('click', function() {
+    var isOpen = $ul.is(':visible'),
+        slideDir = isOpen ? 'slideUp' : 'slideDown',
+        btnText = isOpen ? ogText : 'Bring It Back Up',
+        dur = isOpen ? 200 : 400;
+    $ul.velocity(slideDir, {
+      easing: 'easeOutQuart', 
+      duration: dur,
+      complete: function() {
+        $btn.text(btnText);
+      }
+    });
+  });
+  
+})();
 /**
 
 The MIT License (MIT)
